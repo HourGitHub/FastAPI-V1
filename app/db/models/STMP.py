73650e489b39e4447ff19ab1d8acbf90a7ff403b
HourGitHub <153166683+HourGitHub@users.noreply.github.com>
2024-11-18 11:39:26 +0700

    Update STMP.py

--- app/db/models/STMP.py
+++ app/db/models/STMP.py
@@ -2,26 +2,27 @@
 
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.db.config import Base
-from app.db.models import User
 import pytz
 
-CAMBODIA_TZ = pytz.timezone('Asia/Phnom_Penh')
+# Cambodia timezone
+CAMBODIA_TZ = pytz.timezone("Asia/Phnom_Penh")
 
 class EmailLog(Base):
     __tablename__ = "email_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, index=True)  # The email recipient
-    otp_code = Column(Integer)  # OTP code sent
-    subject = Column(String)  # Subject of the email
-    body = Column(String)  # Body content of the email
-    sent_at = Column(DateTime, default=datetime.utcnow)  # Timestamp of when the email was sent
-    is_verified = Column(Boolean, default=False)  # Verification status (true/false)
-    expiration_time = Column(DateTime, default=lambda: datetime.now(CAMBODIA_TZ), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))  # Link to the user
+    email = Column(String, index=True)
+    otp_code = Column(Integer)
+    subject = Column(String)
+    body = Column(String)
+    sent_at = Column(DateTime, default=lambda: datetime.now(CAMBODIA_TZ))
+    is_verified = Column(Boolean, default=False)
+    expiration_time = Column(DateTime, default=lambda: datetime.now(CAMBODIA_TZ) + timedelta(minutes=10), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="email_logs")
 
     def __repr__(self):
         return f"<EmailLog(id={self.id}, email={self.email}, otp_code={self.otp_code}, sent_at={self.sent_at})>"
+
